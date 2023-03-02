be6ccdb6b1493a2df4cee27317c31ceb1abb5dba
Simi Dickson <Hillaryd97@gmail.com>
2023-03-02 01:09:16 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,11 +21,11 @@ const About = () => {
           <p className="text-2xl lg:text-3xl font-bold tracking-wide lg:text-left text-center py-3 lg:pb-5 text-dark-blue">
             ~ About ~
           </p>
-          <div className="flex lg:hidden border-4 mx-auto items-center justify-center border-t-pink-300 border-b-pink-400 border-r-blue-600 border-l-blue-400 bg-pink-100  rounded-full w-fit lg:h-[26.5rem] lg:w-[26.5rem]">
+          <div className="flex lg:hidden border-4 mx-auto items-center justify-center border-t-pink-300 border-b-pink-400 border-r-blue-600 border-l-blue-400 bg-pink-100  rounded-2xl w-fit lg:h-[26.5rem] lg:w-[26.5rem]">
             <img
               src={ownerImage}
               alt=""
-              className="rounded-full border-4  border-r-pink-300 border-l-pink-400 border-t-blue-600 border-b-blue-400 h-80 lg:h-[26rem] lg:w-[26rem] lg:hover:scale-110"
+              className="rounded-2xl border-4  border-r-pink-300 border-l-pink-400 border-t-blue-600 border-b-blue-400 h-80 lg:h-[26rem] lg:w-[26rem] lg:hover:scale-110"
             />
           </div>
           <h3 className="text-lg text-pink-600 font-bold text-center lg:text-left py-2 lg:text-xl ">
@@ -35,12 +35,14 @@ const About = () => {
           <p className="text-lg text-center lg:text-justify leading-8 pb-2 w-fit">
           Welcome to Rev 3:20 Cakes, where every cake is handmade with love and passion. Our specialty is creating delicious and unique cakes for all of your special occasions. Using only the freshest ingredients, we work closely with our customers to bring their vision to life and make their dream cake a reality. Thank you for considering us, we can't wait to make your special moment sweeter.
           </p>
+          <div className="flex justify-center">
           <a
             href="mailto:hillaryd97@gmail.com"
             className="text-pink-600 w-fit font-semibold text-lg px-7 py-2 rounded-lg hover:bg-pink-200 bg-pink-300 border-white hover:border-2 hover:scale-95 hover:border-pink-600"
           >
             Learn More
           </a>
+          </div>
         </div>
       </div>
     </div>
